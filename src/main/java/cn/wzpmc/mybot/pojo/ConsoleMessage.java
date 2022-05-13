fad0042befed5849835f95cfb627d3d2ba80cefd
Wzp-2008 <3357223099@qq.com>
2022-05-13 21:32:52 +0800

    fix: fix a bug

--- src/main/java/cn/wzpmc/mybot/pojo/ConsoleMessage.java
+++ src/main/java/cn/wzpmc/mybot/pojo/ConsoleMessage.java
@@ -1,14 +1,24 @@
 package cn.wzpmc.mybot.pojo;
 
 import cn.wzpmc.mybot.enums.MessageType;
+import cn.wzpmc.mybot.interfaces.Message;
+import org.slf4j.Logger;
 
 /**
  * @author wzp
  * @version 1.0.0
  * @date 2022/5/13
  */
-public class ConsoleMessage extends Message{
+public class ConsoleMessage extends Message {
     public ConsoleMessage(String content,Console console) {
         super(MessageType.console,content,console);
+    }
+
+    @Override
+    public void reply(String content) {
+        MessageSender sender = this.getSender();
+        Console console = ((Console) sender);
+        Logger logger = console.getLogger();
+        logger.info(content);
     }
 }
