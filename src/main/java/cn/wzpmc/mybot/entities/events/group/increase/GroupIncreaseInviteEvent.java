60729345375825a8437c803e4e652329ed513d04
wzp <minecraftwzpmc@163.com>
2022-07-23 18:41:23 +0800

    fix: fix GroupIncreaseInviteEvent extends GroupDecreasesEvent bug

--- src/main/java/cn/wzpmc/mybot/entities/events/group/increase/GroupIncreaseInviteEvent.java
+++ src/main/java/cn/wzpmc/mybot/entities/events/group/increase/GroupIncreaseInviteEvent.java
@@ -1,6 +1,5 @@
 package cn.wzpmc.mybot.entities.events.group.increase;
 
-import cn.wzpmc.mybot.entities.events.group.decreases.GroupDecreasesEvent;
 import com.alibaba.fastjson2.JSONObject;
 
 /**
@@ -8,7 +7,7 @@ import com.alibaba.fastjson2.JSONObject;
  * @version 1.0.0
  * Created On 2022/6/17 23:27
  */
-public class GroupIncreaseInviteEvent extends GroupDecreasesEvent {
+public class GroupIncreaseInviteEvent extends GroupIncreaseEvent {
     public GroupIncreaseInviteEvent(JSONObject data) {
         super("GroupIncreaseInviteEvent", data);
     }
