38e4845dce3d7519455e617c2139f85a43e8efd6
wzp <minecraftwzpmc@163.com>
2025-10-05 02:23:01 +0800

    pref: make the websocket raw data output to debug level

--- src/main/java/cn/wzpmc/network/PacketHandler.java
+++ src/main/java/cn/wzpmc/network/PacketHandler.java
@@ -38,7 +38,7 @@ public class PacketHandler extends SimpleChannelInboundHandler<TextWebSocketFram
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, TextWebSocketFrame webSocketFrame) {
         String text = webSocketFrame.text();
-        System.out.println(text);
+        log.debug("[WebSocketRawData] {}", text);
         if (!JSON.isValidObject(text)) {
             log.warn("收到了无法处理的WebSocket数据包：{}", text);
             return;
